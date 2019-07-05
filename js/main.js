c1198f263b20f7f3fc427e330d10a89e7e4729fe
Deni <swandriadeny@gmail.com>
2019-07-05 13:50:44 +0200

    add comment on js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 // initialize and add the map
-function initMap(){
-    // your latitude & longtitude location
-    const loc = {}
+function initMAp() {
+  // your latitude & longtitude location
+  const loc = { lat: -28.389722, lng: 153.566334 };
+  // centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  //  the marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
