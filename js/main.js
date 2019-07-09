a11809b39848d0c682e46bef58b9fee4abe132a2
Deni <swandriadeny@gmail.com>
2019-07-09 15:41:39 +0200

    added scrolling barcode

--- js/main.js
+++ js/main.js
@@ -10,16 +10,24 @@ function initMap() {
   //  the marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+//  Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
 
-
-$('#navbar a, .btn') on ('click', function(event) {
-  if (this.hash !==''){
-   event.preventDefoult();
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
     const hash = this.hash;
 
-    $('html, body').animated(
-      { 
-        scrollTop:$(hash).offset().top - 100
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
       },
       800
     );
